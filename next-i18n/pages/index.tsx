dde4e4b68b3f607942664396501a9369ced470a5
WonjunMacstudio <iamjam4944@gmail.com>
2024-08-25 10:14:16 +0900

    i18n예제 상세화

--- next-i18n/pages/index.tsx
+++ next-i18n/pages/index.tsx
@@ -2,13 +2,24 @@ import { useTranslation } from "next-i18next";
 import React from "react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetServerSidePropsContext } from "next";
+import Link from "next/link";
 
 function index() {
+  const { i18n } = useTranslation();
+  const locale = i18n.language;
+
   const { t } = useTranslation("common");
   return (
     <div>
-      index
-      <div>번역언어 : {t("button_okay")}</div>
+      <Link href={"/"} className="hover:underline" locale={"ko"}>
+        한글페이지로 가기
+      </Link>
+      <br></br>
+      <Link href={"/"} className="hover:underline" locale={"en"}>
+        영어페이지로 가기
+      </Link>
+      <h1>현재 언어 : {locale}</h1>
+      <div>번역된 언어 (/public/locales) : {t("button_okay")}</div>
     </div>
   );
 }
